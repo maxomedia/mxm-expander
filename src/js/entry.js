fceec529b976dde3109b1ee7a8b57d6013c01269
tuelsch <tuelsch@gmail.com>
2015-08-12 10:58:34 +0200

    Insert some whitespace

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,26 +1,20 @@
 
-/**
- * Accordion/expander functionality supporting CSS transitions,
- * jQuery fallback or no animation at all.
- * @author Philipp Gfeller, Maxomedia AG
- * @param  {Object} root    Application root, like window in browser environments
- * @param  {Object} factory jQuery instance, imported with available
- *                          module loader (CommonJS, AMD, globals).
- * @return {undefined}
- */
 (function (root, factory) {
 
     // Universal Module Definition
     // https://github.com/umdjs/umd
     if (typeof define === 'function' && define.amd) {
+
         // AMD. Register as an anonymous module.
         define([], factory);
     } else if (typeof exports === 'object') {
+
         // Node. Does not work with strict CommonJS, but
         // only CommonJS-like environments that support module.exports,
         // like Node.
         module.exports = factory();
     } else {
+
         // Browser globals (root is window)
         root.Expander = factory();
     }
